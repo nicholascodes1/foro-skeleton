05e98a9919f84ac1544202b68b1b9bdf935593cd
woof <hinyanryan@gmail.com>
2026-04-25 23:17:07 +0800

    background now has parallax by importing react-parallax npm package.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Parallax } from "react-parallax";
 import HeroWelcome from "@/components/marketing-page/Welcome";
 import HeroAboutUs from "@/components/marketing-page/AboutUs/AboutUs";
 import HeroFourWays from "@/components/marketing-page/FourWaysSection/HeroFourWays";
@@ -5,18 +8,28 @@ import HeroUsedByStudents from "@/components/marketing-page/UsedByStudents/UsedB
 
 export default function Page() {
   return (
-    <main className="bg-[url('/images/hero-catalogue-bg.png')] bg-contain bg-no-repeat">
-      {" "}
-      <HeroWelcome />
-      <div className="bg-white">
-        <div className="mt-[375px]">
-          <HeroAboutUs />
+    <main>
+      {/* Wrap the ENTIRE page content inside the Parallax component */}
+      <Parallax 
+        bgImage="/images/hero-catalogue-bg.png" 
+        strength={800} // You might want to increase strength since the scroll is longer now
+        bgImageStyle={{ objectFit: "contain", objectPosition: "top" }}
+      >
+        
+        {/* Top Hero Section */}
+        <div className="pt-24">
+          <HeroWelcome />
         </div>
-        <HeroFourWays />
-        <HeroUsedByStudents />
-      </div>
+
+        <div className="relative z-20">
+          <div className="mt-[375px]">
+            <HeroAboutUs />
+          </div>
+          <HeroFourWays />
+          <HeroUsedByStudents />
+        </div>
+
+      </Parallax>
     </main>
   );
 }
-
-
