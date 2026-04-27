c44f3ead66fc4eafb4a0dbd7cf70d2fd428abb03
Nicholas <darthshroda28@gmail.com>
2026-04-27 15:29:21 +0800

    Fixed parralax effect on landing page not working when coming from other pages.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,24 +5,29 @@ import HeroAboutUs from "@/components/marketing-page/AboutUs/AboutUs";
 import HeroFourWays from "@/components/marketing-page/FourWaysSection/HeroFourWays";
 import HeroUsedByStudents from "@/components/marketing-page/UsedByStudents/UsedByStudents";
 import Parallax from "@/components/Parallax"
+import { usePathname } from "next/navigation";
 
 export default function Page() {
+  const pathname = usePathname();
+
   return (
     <main>
-      <Parallax bgImage="/images/hero-catalogue-bg.png" strength={1000}>
-        
+      <Parallax
+        bgImage="/images/hero-catalogue-bg.png"
+        strength={1000}
+        key={pathname}
+      >
         <div className="pt-24">
           <HeroWelcome />
         </div>
 
         <div className="relative z-20">
-          <div className="mt-[450px]">
+          <div className="mt-[400px]">
             <HeroAboutUs />
           </div>
           <HeroFourWays />
           <HeroUsedByStudents />
         </div>
-
       </Parallax>
     </main>
   );
